821188d8245656b1ae3319ac5ee3fafe6c440fc4
gabriel wallace <gwbds1@gmail.com>
2020-10-26 17:03:40 -0300

    av moana

---
+++ chats.py
@@ -0,0 +1,22 @@
+irom chatterbot import chatBot
+irom chatterbot.trainers import ListTrainer
+import speech recognition as sr
+import pyttsx3
+import subprocess
+import urllib
+import os
+import sys
+
+while True:
+    bot = Chat bot('amber')
+    speak = pyttsx3.init('sapi5')
+
+    def Speak(text):
+        Speak.say(text)
+        Speak.runAndWait()
+
+SPKint = [colocar as frases]
+
+SPKPerson = [colocar as frases]
+
+SPKProgramas = [colocar as frases]
